42de45b1a73466188290dabc305018298eed5f83
Saintmonza <lrmonza@gmail.com>
2021-10-03 11:19:46 +0100

    Review - controller and sweep created

---
+++ db/reviewSweep.js
@@ -0,0 +1,11 @@
+import Product from '../models/product.js'
+import { productSeedData } from './productSeedData.js'
+// Once seed is created import helpers
+
+async function reviewSweep() {
+  try {
+    // update once seed is created
+  } catch (err) {
+    next()
+  }
+}
